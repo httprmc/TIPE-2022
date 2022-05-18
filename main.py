8c4c6042eeb84787c397ea2cb6ac0c9c9982adac
Loulou <louis.delyon.si@gmail.com>
2022-05-18 13:04:56 +0200

    marche toujours pas

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 # IMPORTS #
-from PIL import Image, ImageOps, ImageDraw
+from PIL import Image, ImageOps, ImageDraw, ImageFilter
 import numpy as np
 from math import cos, sin, pi
 
 import os
 
 # CONSTANTES #
-k = .9
-k1 = .7
+k = .8
+k1 = .9
 
 # PARAMETRES #
-niteration = 10
+niteration = 75
 npoints = 100
 r = 50
 
 # PRECALCULS #
 img = Image.open("img.jpg")
 img = ImageOps.grayscale(img)
+#img = img.filter(ImageFilter.BLUR)
 
 img.save("gray.jpg")
 
@@ -34,23 +35,23 @@ for i in range(len(forcex)):
     for j in range(len(forcex[0])):
         l = (forcex[i,j]**2 + forcey[i,j]**2)**.5
         if l != 0:
-            forcex[i,j] *= -k/l
-            forcey[i,j] *= -k/l
+            forcex[i,j] *= k/l
+            forcey[i,j] *= k/l
 
 #Courbe initiale
-v = [(round(center[0]+r*cos(theta)), round(center[1]+r*sin(theta))) for theta in np.linspace(0, 2*pi, npoints+1)][:-1]
+v = [(center[0]+r*cos(theta), center[1]+r*sin(theta)) for theta in np.linspace(0, 2*pi, npoints+1)][:-1]
 
 def inflate(v, forcex, forcey):
     "Applique la force de gonflement à chaque point de la courbe"
     l = len(v)
     newv = l*[(0,0)]
     for s,(x,y) in enumerate(v):
-        fx, fy = forcex[y,x], forcey[y,x]
+        fx, fy = forcex[round(y),round(x)], forcey[round(y),round(x)]
 
         #Calcul du vecteur normal
         ny = -(v[(s+1)%l][0] - v[(s-1)%l][0])
         nx = v[(s+1)%l][1] - v[(s-1)%l][1]
-        module = max(nx,ny)#(nx**2 + ny**2)**.5
+        module = (nx**2 + ny**2)**.5
         if module==0 :
             newv[s] = v[s]
             continue
@@ -59,7 +60,7 @@ def inflate(v, forcex, forcey):
 
         #Calcul du nouveau point
         dx, dy = k1*nx + fx, k1*ny + fy
-        newv[s] = (round(x+dx), round(y+dy))
+        newv[s] = (x+dx, y+dy)
 
     return newv
 
